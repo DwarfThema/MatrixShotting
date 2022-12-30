8a955afe2ac7d28be280bcefa0920c60884f7dfe
Junho Park <dwarfthema@gmail.com>
2022-12-30 23:52:47 +0900

    player move

--- main.js
+++ main.js
@@ -18,6 +18,32 @@ const loadImage = () => {
     bug.src = "resources/bug.png";
     gameOver.src = "resources/gameOver.png";
 };
+let inputManager = {};
+const inputKeyboardLinstener = () => {
+    document.addEventListener("keydown", (e) => {
+        inputManager[e.key] = true;
+        console.log("keydown : ", inputManager);
+    });
+    document.addEventListener("keyup", (e) => {
+        delete inputManager[e.key];
+        console.log("keyup : ", inputManager);
+    });
+};
+let speed = 2;
+const inputUpdate = () => {
+    if ("ArrowRight" in inputManager) {
+        hackerX += speed;
+    }
+    else if ("ArrowLeft" in inputManager) {
+        hackerX -= speed;
+    }
+    if (hackerX <= -25) {
+        hackerX = -25;
+    }
+    else if (hackerX >= canvas.width - 75) {
+        hackerX = canvas.width - 75;
+    }
+};
 //해커 좌표
 let hackerX = canvas.width / 2 - 50; //중앙값
 let hackerY = canvas.height - 100; // 아래값
@@ -27,8 +53,10 @@ const render = () => {
 };
 const runtimeUpdate = () => {
     render();
+    inputUpdate();
     requestAnimationFrame(runtimeUpdate);
     // requestAnimationFrame 의 재귀를 통해 update life cycle 을 만들 수 있다.
 };
 loadImage();
+inputKeyboardLinstener();
 runtimeUpdate();
--- main.ts
+++ main.ts
@@ -22,6 +22,34 @@ const loadImage = (): void => {
   gameOver.src = "resources/gameOver.png";
 };
 
+let inputManager: any = {};
+
+const inputKeyboardLinstener = (): void => {
+  document.addEventListener("keydown", (e: KeyboardEvent) => {
+    inputManager[e.key] = true;
+    console.log("keydown : ", inputManager);
+  });
+  document.addEventListener("keyup", (e: KeyboardEvent) => {
+    delete inputManager[e.key];
+    console.log("keyup : ", inputManager);
+  });
+};
+
+let speed: number = 2;
+const inputUpdate = (): void => {
+  if ("ArrowRight" in inputManager) {
+    hackerX += speed;
+  } else if ("ArrowLeft" in inputManager) {
+    hackerX -= speed;
+  }
+
+  if (hackerX <= -25) {
+    hackerX = -25;
+  } else if (hackerX >= canvas.width - 75) {
+    hackerX = canvas.width - 75;
+  }
+};
+
 //해커 좌표
 let hackerX: number = canvas.width / 2 - 50; //중앙값
 let hackerY: number = canvas.height - 100; // 아래값
@@ -33,9 +61,11 @@ const render = (): void => {
 
 const runtimeUpdate = (): void => {
   render();
+  inputUpdate();
   requestAnimationFrame(runtimeUpdate);
   // requestAnimationFrame 의 재귀를 통해 update life cycle 을 만들 수 있다.
 };
 
 loadImage();
+inputKeyboardLinstener();
 runtimeUpdate();
