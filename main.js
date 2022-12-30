6e00ee4294aa86c7e37fa094968ab397b77ce9d5
Junho Park <dwarfthema@gmail.com>
2022-12-30 23:11:42 +0900

    npm init / tsconfig.json

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
+"use strict";
 //--canvas setting
-var canvas = document.createElement("canvas");
+let canvas = document.createElement("canvas");
 var ctx = canvas.getContext("2d");
 canvas.width = 400;
 canvas.height = 700;
 // 각각 픽셀로 적용됨
 document.body.appendChild(canvas);
-var bg = new Image();
-var hacker = new Image();
-var bullet = new Image();
-var bug = new Image();
-var gameOver = new Image();
-var loadImage = function () {
+const bg = new Image();
+const hacker = new Image();
+const bullet = new Image();
+const bug = new Image();
+const gameOver = new Image();
+const loadImage = () => {
     bg.src = "resources/bg.webp";
     hacker.src = "resources/hacker.png";
     bullet.src = "resources/bullet.png";
@@ -18,13 +19,13 @@ var loadImage = function () {
     gameOver.src = "resources/gameOver.png";
 };
 //해커 좌표
-var hackerX = canvas.width / 2 - 50; //중앙값
-var hackerY = canvas.height - 100; // 아래값
-var render = function () {
+let hackerX = canvas.width / 2 - 50; //중앙값
+let hackerY = canvas.height - 100; // 아래값
+const render = () => {
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(hacker, hackerX, hackerY);
 };
-var runtimeUpdate = function () {
+const runtimeUpdate = () => {
     render();
     requestAnimationFrame(runtimeUpdate);
     // requestAnimationFrame 의 재귀를 통해 update life cycle 을 만들 수 있다.
