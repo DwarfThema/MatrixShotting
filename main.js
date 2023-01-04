c7ef20e5956c7a693dd41cf0b3186e398a90e90e
Junho Park <dwarfthema@gmail.com>
2023-01-04 22:17:37 +0900

    shoot bullet

--- main.js
+++ main.js
@@ -8,13 +8,13 @@ canvas.height = 700;
 document.body.appendChild(canvas);
 const bg = new Image();
 const hacker = new Image();
-const bullet = new Image();
+const bulletImg = new Image();
 const bug = new Image();
 const gameOver = new Image();
 const loadImage = () => {
     bg.src = "resources/bg.webp";
     hacker.src = "resources/hacker.png";
-    bullet.src = "resources/bullet.png";
+    bulletImg.src = "resources/bullet.png";
     bug.src = "resources/bug.png";
     gameOver.src = "resources/gameOver.png";
 };
@@ -22,12 +22,30 @@ let inputManager = {};
 const inputKeyboardLinstener = () => {
     document.addEventListener("keydown", (e) => {
         inputManager[e.key] = true;
-        console.log("keydown : ", inputManager);
     });
     document.addEventListener("keyup", (e) => {
         delete inputManager[e.key];
-        console.log("keyup : ", inputManager);
+        // spaceBar와 반응해서 총알 생성
+        if (e.key === " ") {
+            createBullet();
+        }
     });
+};
+//bulletArr 안에 Bullet Obj를 넣을 계획
+const bulletArr = [];
+class Bullet {
+    constructor(x, y) {
+        this.x = x + 25;
+        this.y = y - 15;
+    }
+    update() {
+        this.y -= 3;
+    }
+}
+//총알 생성 함수
+const createBullet = () => {
+    let newBullet = new Bullet(hackerX, hackerY);
+    bulletArr.push(newBullet);
 };
 let speed = 2;
 const inputUpdate = () => {
@@ -50,10 +68,18 @@ let hackerY = canvas.height - 100; // 아래값
 const render = () => {
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(hacker, hackerX, hackerY);
+    //BulletImg 를 로드
+    for (let i = 0; i < bulletArr.length; i++) {
+        ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(bulletImg, bulletArr[i].x, bulletArr[i].y, 40, 40);
+    }
 };
 const runtimeUpdate = () => {
     render();
     inputUpdate();
+    //총알 위로 발사시키는 함수 Update 실행
+    for (let i = 0; i < bulletArr.length; i++) {
+        bulletArr[i].update();
+    }
     requestAnimationFrame(runtimeUpdate);
     // requestAnimationFrame 의 재귀를 통해 update life cycle 을 만들 수 있다.
 };
--- main.ts
+++ main.ts
@@ -10,14 +10,14 @@ document.body.appendChild(canvas);
 
 const bg: HTMLImageElement = new Image();
 const hacker: HTMLImageElement = new Image();
-const bullet: HTMLImageElement = new Image();
+const bulletImg: HTMLImageElement = new Image();
 const bug: HTMLImageElement = new Image();
 const gameOver: HTMLImageElement = new Image();
 
 const loadImage = (): void => {
   bg.src = "resources/bg.webp";
   hacker.src = "resources/hacker.png";
-  bullet.src = "resources/bullet.png";
+  bulletImg.src = "resources/bullet.png";
   bug.src = "resources/bug.png";
   gameOver.src = "resources/gameOver.png";
 };
@@ -27,12 +27,36 @@ let inputManager: any = {};
 const inputKeyboardLinstener = (): void => {
   document.addEventListener("keydown", (e: KeyboardEvent) => {
     inputManager[e.key] = true;
-    console.log("keydown : ", inputManager);
   });
   document.addEventListener("keyup", (e: KeyboardEvent) => {
     delete inputManager[e.key];
-    console.log("keyup : ", inputManager);
+
+    // spaceBar와 반응해서 총알 생성
+    if (e.key === " ") {
+      createBullet();
+    }
   });
+};
+
+//bulletArr 안에 Bullet Obj를 넣을 계획
+const bulletArr: any = [];
+
+class Bullet {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x + 25;
+    this.y = y - 15;
+  }
+  update() {
+    this.y -= 3;
+  }
+}
+
+//총알 생성 함수
+const createBullet = () => {
+  let newBullet = new Bullet(hackerX, hackerY);
+  bulletArr.push(newBullet);
 };
 
 let speed: number = 2;
@@ -57,11 +81,22 @@ let hackerY: number = canvas.height - 100; // 아래값
 const render = (): void => {
   ctx?.drawImage(bg, 0, 0, canvas.width, canvas.height);
   ctx?.drawImage(hacker, hackerX, hackerY);
+
+  //BulletImg 를 로드
+  for (let i = 0; i < bulletArr.length; i++) {
+    ctx?.drawImage(bulletImg, bulletArr[i].x, bulletArr[i].y, 40, 40);
+  }
 };
 
 const runtimeUpdate = (): void => {
   render();
   inputUpdate();
+
+  //총알 위로 발사시키는 함수 Update 실행
+  for (let i = 0; i < bulletArr.length; i++) {
+    bulletArr[i].update();
+  }
+
   requestAnimationFrame(runtimeUpdate);
   // requestAnimationFrame 의 재귀를 통해 update life cycle 을 만들 수 있다.
 };
