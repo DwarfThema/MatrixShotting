2ac0c682fcf61e49f185514293283fb0a84c3b92
Junho Park <dwarfthema@gmail.com>
2023-01-06 19:59:24 +0900

    fin

--- main.js
+++ main.js
@@ -1,22 +1,26 @@
 "use strict";
 //--canvas setting
 let canvas = document.createElement("canvas");
-var ctx = canvas.getContext("2d");
+let ctx = canvas.getContext("2d");
 canvas.width = 400;
 canvas.height = 700;
 // 각각 픽셀로 적용됨
 document.body.appendChild(canvas);
+let gameOverBool = false;
+let score = 0;
 const bg = new Image();
 const hacker = new Image();
 const bulletImg = new Image();
 const bug = new Image();
 const gameOver = new Image();
+const boomImg = new Image();
 const loadImage = () => {
     bg.src = "resources/bg.webp";
     hacker.src = "resources/hacker.png";
     bulletImg.src = "resources/bullet.png";
     bug.src = "resources/bug.png";
     gameOver.src = "resources/gameOver.png";
+    boomImg.src = "resources/boom.png";
 };
 let inputManager = {};
 const inputKeyboardLinstener = () => {
@@ -37,15 +41,53 @@ class Bullet {
     constructor(x, y) {
         this.x = x + 25;
         this.y = y - 15;
+        this.alive = true;
     }
     update() {
         this.y -= 3;
+    }
+    // 맞았는지 체크하는 함수. this.alive 로 확인 가능하다.
+    checkHit() {
+        for (let i = 0; i < enemyArr.length; i++) {
+            if (this.y <= enemyArr[i].y &&
+                this.x >= enemyArr[i].x &&
+                this.x <= enemyArr[i].x + 40) {
+                score++;
+                this.alive = false;
+                ctx.drawImage(boomImg, this.x - 10, this.y - 10, 60, 60);
+                enemyArr.splice(i, 1);
+            }
+        }
     }
 }
 //총알 생성 함수
 const createBullet = () => {
     let newBullet = new Bullet(hackerX, hackerY);
     bulletArr.push(newBullet);
+};
+function randomFn(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+const enemyArr = [];
+class Enemy {
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    }
+    update() {
+        this.y += 2;
+        if (this.y >= canvas.height - 40) {
+            gameOverBool = true;
+        }
+    }
+}
+//적군 생성 함수
+const createEnemy = () => {
+    setInterval(function () {
+        let newEnemy = new Enemy(randomFn(0, canvas.width - 40), 0);
+        enemyArr.push(newEnemy);
+    }, 1000);
+    // setInterval 은 특정 시간마다 호출하고싶은 함수를 정할 수 있음
 };
 let speed = 2;
 const inputUpdate = () => {
@@ -68,21 +110,43 @@ let hackerY = canvas.height - 100; // 아래값
 const render = () => {
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(bg, 0, 0, canvas.width, canvas.height);
     ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(hacker, hackerX, hackerY);
+    ctx === null || ctx === void 0 ? void 0 : ctx.fillText(`Score : ${score}`, 20, 20);
+    ctx.fillStyle = "white";
+    ctx.font = "20px arial";
     //BulletImg 를 로드
     for (let i = 0; i < bulletArr.length; i++) {
-        ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(bulletImg, bulletArr[i].x, bulletArr[i].y, 40, 40);
+        if (bulletArr[i].alive) {
+            ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(bulletImg, bulletArr[i].x, bulletArr[i].y, 40, 40);
+        }
+    }
+    //EnemyImg 를 로드
+    for (let i = 0; i < enemyArr.length; i++) {
+        ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(bug, enemyArr[i].x, enemyArr[i].y, 40, 40);
     }
 };
 const runtimeUpdate = () => {
-    render();
-    inputUpdate();
-    //총알 위로 발사시키는 함수 Update 실행
-    for (let i = 0; i < bulletArr.length; i++) {
-        bulletArr[i].update();
+    if (!gameOverBool) {
+        render();
+        inputUpdate();
+        //총알 위로 발사시키는 함수 Update 실행
+        for (let i = 0; i < bulletArr.length; i++) {
+            if (bulletArr[i].alive) {
+                //bullet 이 살아있다면!
+                bulletArr[i].update();
+                bulletArr[i].checkHit();
+            }
+        }
+        for (let i = 0; i < enemyArr.length; i++) {
+            enemyArr[i].update();
+        }
+        requestAnimationFrame(runtimeUpdate);
+        // requestAnimationFrame 의 재귀를 통해 update life cycle 을 만들 수 있다.
     }
-    requestAnimationFrame(runtimeUpdate);
-    // requestAnimationFrame 의 재귀를 통해 update life cycle 을 만들 수 있다.
+    else {
+        ctx === null || ctx === void 0 ? void 0 : ctx.drawImage(gameOver, canvas.width / 2 - 140, 150, 300, 150);
+    }
 };
 loadImage();
 inputKeyboardLinstener();
+createEnemy();
 runtimeUpdate();
--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 //--canvas setting
-let canvas: HTMLCanvasElement = document.createElement("canvas");
-var ctx: CanvasRenderingContext2D | null = canvas.getContext("2d");
+let canvas = document.createElement("canvas") as HTMLCanvasElement;
+let ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
 
 canvas.width = 400;
 canvas.height = 700;
@@ -8,11 +8,15 @@ canvas.height = 700;
 
 document.body.appendChild(canvas);
 
+let gameOverBool = false;
+let score = 0;
+
 const bg: HTMLImageElement = new Image();
 const hacker: HTMLImageElement = new Image();
 const bulletImg: HTMLImageElement = new Image();
 const bug: HTMLImageElement = new Image();
 const gameOver: HTMLImageElement = new Image();
+const boomImg = new Image() as HTMLImageElement;
 
 const loadImage = (): void => {
   bg.src = "resources/bg.webp";
@@ -20,6 +24,7 @@ const loadImage = (): void => {
   bulletImg.src = "resources/bullet.png";
   bug.src = "resources/bug.png";
   gameOver.src = "resources/gameOver.png";
+  boomImg.src = "resources/boom.png";
 };
 
 let inputManager: any = {};
@@ -44,19 +49,68 @@ const bulletArr: any = [];
 class Bullet {
   x: number;
   y: number;
+  alive: boolean;
   constructor(x: number, y: number) {
     this.x = x + 25;
     this.y = y - 15;
+    this.alive = true;
   }
   update() {
     this.y -= 3;
   }
+
+  // 맞았는지 체크하는 함수. this.alive 로 확인 가능하다.
+  checkHit() {
+    for (let i = 0; i < enemyArr.length; i++) {
+      if (
+        this.y <= enemyArr[i].y &&
+        this.x >= enemyArr[i].x &&
+        this.x <= enemyArr[i].x + 40
+      ) {
+        score++;
+        this.alive = false;
+        ctx.drawImage(boomImg, this.x - 10, this.y - 10, 60, 60);
+        enemyArr.splice(i, 1);
+      }
+    }
+  }
 }
 
 //총알 생성 함수
-const createBullet = () => {
+const createBullet = (): void => {
   let newBullet = new Bullet(hackerX, hackerY);
   bulletArr.push(newBullet);
+};
+
+function randomFn(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+const enemyArr: any = [];
+
+class Enemy {
+  x: number;
+  y: number;
+  constructor(x: number, y: number) {
+    this.x = x;
+    this.y = y;
+  }
+  update() {
+    this.y += 2;
+
+    if (this.y >= canvas.height - 40) {
+      gameOverBool = true;
+    }
+  }
+}
+
+//적군 생성 함수
+const createEnemy = (): void => {
+  setInterval(function () {
+    let newEnemy = new Enemy(randomFn(0, canvas.width - 40), 0);
+    enemyArr.push(newEnemy);
+  }, 1000);
+  // setInterval 은 특정 시간마다 호출하고싶은 함수를 정할 수 있음
 };
 
 let speed: number = 2;
@@ -82,25 +136,48 @@ const render = (): void => {
   ctx?.drawImage(bg, 0, 0, canvas.width, canvas.height);
   ctx?.drawImage(hacker, hackerX, hackerY);
 
+  ctx?.fillText(`Score : ${score}`, 20, 20);
+  ctx.fillStyle = "white";
+  ctx.font = "20px arial";
+
   //BulletImg 를 로드
   for (let i = 0; i < bulletArr.length; i++) {
-    ctx?.drawImage(bulletImg, bulletArr[i].x, bulletArr[i].y, 40, 40);
+    if (bulletArr[i].alive) {
+      ctx?.drawImage(bulletImg, bulletArr[i].x, bulletArr[i].y, 40, 40);
+    }
+  }
+  //EnemyImg 를 로드
+  for (let i = 0; i < enemyArr.length; i++) {
+    ctx?.drawImage(bug, enemyArr[i].x, enemyArr[i].y, 40, 40);
   }
 };
 
 const runtimeUpdate = (): void => {
-  render();
-  inputUpdate();
+  if (!gameOverBool) {
+    render();
+    inputUpdate();
 
-  //총알 위로 발사시키는 함수 Update 실행
-  for (let i = 0; i < bulletArr.length; i++) {
-    bulletArr[i].update();
+    //총알 위로 발사시키는 함수 Update 실행
+    for (let i = 0; i < bulletArr.length; i++) {
+      if (bulletArr[i].alive) {
+        //bullet 이 살아있다면!
+        bulletArr[i].update();
+        bulletArr[i].checkHit();
+      }
+    }
+
+    for (let i = 0; i < enemyArr.length; i++) {
+      enemyArr[i].update();
+    }
+
+    requestAnimationFrame(runtimeUpdate);
+    // requestAnimationFrame 의 재귀를 통해 update life cycle 을 만들 수 있다.
+  } else {
+    ctx?.drawImage(gameOver, canvas.width / 2 - 140, 150, 300, 150);
   }
-
-  requestAnimationFrame(runtimeUpdate);
-  // requestAnimationFrame 의 재귀를 통해 update life cycle 을 만들 수 있다.
 };
 
 loadImage();
 inputKeyboardLinstener();
+createEnemy();
 runtimeUpdate();
